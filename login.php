6373ad0f89a8a9444dcb7d9acd25841128dd2e74
Rahulmac01 <machhirahul1212@gmail.com>
2025-08-29 17:41:39 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+include 'db.php';
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email='$email'";
+    $result = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_assoc($result);
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['username'] = $user['username'];
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Invalid Email or Password!";
+    }
+}
+?>
+
+<!-- Login Form -->
+<form method="POST">
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Login</button>
+</form>
